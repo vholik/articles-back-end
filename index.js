5235e4978782b64cbdc4abe462b2e654b718d586
vholik <1viktorholik@gmail.com>
2022-08-20 00:47:16 +0200

    ready to deploy

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ import { handleValidationErrors, checkAuth } from "./utils/index.js";
 import { UserController, PostController } from "./controllers/index.js";
 
 mongoose
-  .connect(
-    "mongodb+srv://admin:wwwwww@cluster0.mphiejk.mongodb.net/blog?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("DB ok"))
   .catch((err) => console.log("DB error", err));
 
